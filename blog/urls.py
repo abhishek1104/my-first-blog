ac2dd766902bd1495340d12ab245c0fd951caae9
abhishek <abhishek1104@gmail.com>
2015-06-15 18:12:18 +0530

    2ndlast update

--- blog/urls.py
+++ blog/urls.py
@@ -6,7 +6,8 @@ urlpatterns=[
     url(r'^$',views.index,name='index'),
     url(r'^about2/$',views.index1,name='index1'),
     url(r'^posts/$',views.post_list,name='post_list'),
-    url(r'^posts/(?P<pk>[0-9]+)/$',views.post_detail),
+    url(r'^post/(?P<pk>[0-9]+)/$',views.post_detail),
     url(r'^post/new/$',views.post_new,name='post_new'),
+    url(r'^post/(?P<pk>[0-9]+)/edit/$',views.post_edit,name='post_edit'),
 ]
 
--- blog/views.py
+++ blog/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render,get_object_or_404
+from django.shortcuts import render,get_object_or_404,redirect
 from django.http import HttpResponse
 from .models import Post #here . means current directory (i.e. blog.models )
 from django.utils import timezone
@@ -22,6 +22,32 @@ def post_detail(request,pk):
     return render(request,'blog/post_detail.html',{'post': post})
 
 def post_new(request):
-    form=PostForm()
-    return render(request,'blog/post_edit.html',{'form': form})
+    if request.method=="POST":
+        form=PostForm(request.POST)
+        if form.is_valid():
+            post =form.save(commit=False)
+            post.author=request.user
+            post.save()
 
+            return redirect('blog.views.post_detail',pk=post.pk)
+    else:
+        form=PostForm()
+
+    return render(request,'blog/post_edit.html',{'form':form})
+
+
+
+def post_edit(request,pk=post.pk):
+    form=get_object_or_404(Post,pk=pk)
+    if request.method=="POST":
+        form=PostForm(request.POST,instance=post)
+        if form.is_valid():
+            post=form.save(commit=False)
+            post.author=request.user
+            post.save()
+            return redirect('blog.views.post_detail',pk=post.pk)
+
+        else:
+            form=PostForm(instance=post)
+
+    return render(request, 'blog/post_edit.html',{'form':form})
