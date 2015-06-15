14ffd3c21f35cd664113bf7e68a0a9deb07f0922
abhishek <abhishek1104@gmail.com>
2015-06-15 14:01:04 +0530

    form updated

--- blog/urls.py
+++ blog/urls.py
@@ -6,6 +6,7 @@ urlpatterns=[
     url(r'^$',views.index,name='index'),
     url(r'^about2/$',views.index1,name='index1'),
     url(r'^posts/$',views.post_list,name='post_list'),
-    url(r'^posts/(?P<pk>[0-9]+)/$',views.post_detail)
+    url(r'^posts/(?P<pk>[0-9]+)/$',views.post_detail),
+    url(r'^post/new/$',views.post_new,name='post_new'),
 ]
 
--- blog/views.py
+++ blog/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render,get_object_or_404
 from django.http import HttpResponse
 from .models import Post #here . means current directory (i.e. blog.models )
 from django.utils import timezone
+from .forms import PostForm
 # Create your views here.
 
 def index(request):
@@ -19,3 +20,8 @@ def post_list(request):
 def post_detail(request,pk):
     post=get_object_or_404(Post, pk=pk)
     return render(request,'blog/post_detail.html',{'post': post})
+
+def post_new(request):
+    form=PostForm()
+    return render(request,'blog/post_edit.html',{'form': form})
+
